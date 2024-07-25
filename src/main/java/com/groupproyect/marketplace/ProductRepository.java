170cdbfeb628e96dce9f7a377cc64fd6bad969c1
AracelyRios <168043362+AracelyRios@users.noreply.github.com>
2024-07-25 02:09:54 -0500

    Add files via upload

---
+++ src/main/java/com/groupproyect/marketplace/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.groupproyect.marketplace.repository.product;
+
+import com.groupproyect.marketplace.model.product.Product;
+import com.groupproyect.marketplace.repository.BaseRepository;
+
+
+public interface ProductRepository extends BaseRepository<Product> {
+
+
+} 
