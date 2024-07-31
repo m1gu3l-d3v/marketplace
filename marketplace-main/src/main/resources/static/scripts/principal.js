7ad9c276b06a3cd90861e228ffc76e878a302e3d
Jowerly <131895977+Jowerly@users.noreply.github.com>
2024-07-30 23:32:19 -0500

    Update principal.js

--- marketplace-main/src/main/resources/static/scripts/principal.js
+++ marketplace-main/src/main/resources/static/scripts/principal.js
@@ -41,3 +41,32 @@
     }
     menu.addEventListener('click', () => list.classList.toggle('menu_links--show'));
 })();
+function App(){}
+    window.onload = function(event){
+        var app = new App();
+        window.app = app;
+    }
+    App.prototype.processingButton = function(event){
+        const btn = event.currentTarget;
+        const carruselList = event.currentTarget.parentNode;
+        const track = event.currentTarget.parentNode.querySelector('#track');
+        const carrusel = track.querySelectorAll('.carrusel');
+        const carruselWidth = carrusel[0].offsetWidth;
+        const listWidth = carruselList.offsetWidth;
+        const trackWidth = track.offsetWidth;
+
+        track.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0,-2) * -1);
+
+        btn.dataset.button == "button-prev" ? prevAction(leftPosition,carruselWidth, track) : nextAction(leftPosition, trackWidth , listWidth, carruselWidth, track)
+    }
+
+    let prevAction = (leftPosition, carruselWidth, track) => {
+        if (leftPosition > 0) {
+            track.style.left = `${-1 * (leftPosition - carruselWidth)}px`;
+        }
+    }
+    let nextAction = (leftPosition, trackWidth, carruselWidth ,listWidth , track) => {
+        if (leftPosition < (trackWidth - listWidth)) {
+            track.style.left = `${-1 * (leftPosition + carruselWidth)}px`;
+        }
+    }
