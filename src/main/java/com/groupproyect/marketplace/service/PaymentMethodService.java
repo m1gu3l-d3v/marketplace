ed38635771a25225eb83a4a4ea9bc8996f9ae100
AracelyRios <168043362+AracelyRios@users.noreply.github.com>
2024-08-04 19:56:17 -0500

    Add files via upload

---
+++ src/main/java/com/groupproyect/marketplace/service/PaymentMethodService.java
@@ -0,0 +1,20 @@
+package com.groupproyect.marketplace.service.delivery;
+
+import org.springframework.stereotype.Service;
+
+import com.groupproyect.marketplace.model.delivery.DeliveryMethod;
+import com.groupproyect.marketplace.model.purchase.PaymentMethod;
+import com.groupproyect.marketplace.repository.delivery.DeliveryMethodRepository;
+import com.groupproyect.marketplace.repository.delivery.PaymentMethodRepository;
+import com.groupproyect.marketplace.service.BaseService;
+
+@Service
+public class PaymentMethodService  extends BaseService<PaymentMethod> {
+    @SuppressWarnings("unused")
+    private final PaymentMethodRepository paymentMethodRepository;
+
+    public PaymentMethodService(PaymentMethodRepository paymentMethodRepository) {
+        super(paymentMethodRepository);
+        this.paymentMethodRepository = paymentMethodRepository;
+    }
+}
