77faee136e2e22a7bb203dddf7ac287e7f211480
AracelyRios <168043362+AracelyRios@users.noreply.github.com>
2024-08-04 19:54:54 -0500

    Add files via upload

---
+++ src/main/java/com/groupproyect/marketplace/repository/PaymentMethodRepository.java
@@ -0,0 +1,8 @@
+package com.groupproyect.marketplace.repository.delivery;
+
+import com.groupproyect.marketplace.model.purchase.PaymentMethod;
+import com.groupproyect.marketplace.repository.BaseRepository;
+
+public interface PaymentMethodRepository extends BaseRepository<PaymentMethod>{
+    
+}
