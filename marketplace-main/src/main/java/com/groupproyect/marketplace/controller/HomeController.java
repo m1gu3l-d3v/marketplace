5799f4ebc62a5c0e11b87493294c10fe0896265e
Jowerly <131895977+Jowerly@users.noreply.github.com>
2024-08-05 21:21:46 -0500

    Update HomeController.java

--- marketplace-main/src/main/java/com/groupproyect/marketplace/controller/HomeController.java
+++ marketplace-main/src/main/java/com/groupproyect/marketplace/controller/HomeController.java
@@ -1,39 +1,43 @@
 package com.groupproyect.marketplace.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.groupproyect.marketplace.model.cache.ProductClientCache;
-import com.groupproyect.marketplace.service.cache.ProductClientCacheService;
+import com.groupproyect.marketplace.model.store.Store;
 import com.groupproyect.marketplace.service.category.CategoryOneService;
 import com.groupproyect.marketplace.service.product.ProductService;
-import com.groupproyect.marketplace.service.user.ClientService;
+import com.groupproyect.marketplace.service.store.StoreService;
+import com.groupproyect.marketplace.service.user.SellerService;
 
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("")
 public class HomeController {
-  @SuppressWarnings("unused")
   private final ProductService productService;
   private final CategoryOneService categoryOneService;
-  private final ClientService clientService;
 
-  public  HomeController(ProductService productService, CategoryOneService categoryOneService, ClientService clientService) {
+  public  HomeController(ProductService productService,CategoryOneService categoryOneService) {
     this.productService = productService;
     this.categoryOneService = categoryOneService;
-    this.clientService = clientService;
   }
+
+  @Autowired
+  SellerService sellerService;
 
   @GetMapping({ "", "/", "/index", "/index/", "/home", "/home/" })
   public String home(HttpSession httpSession, Model model) {
     Long idUser = (Long) httpSession.getAttribute("idUser");
+
     model.addAttribute("categoriesOne", categoryOneService.findAll());
     model.addAttribute("user", idUser);
+    model.addAttribute("products", productService.findAll());
     return "index.jsp";
+    
   }
 }
